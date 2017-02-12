2aea672778ae79cd4d14bd7fe5790c0a2484a86d
Hong <keepthemonochrome@gmail.com>
2017-02-12 00:10:59 -0800

    (feat/deviceFrontend) Fetch message from api, notify user that a message arrived

--- com.enhance.da.breadcrumbs/app/worker.js
+++ com.enhance.da.breadcrumbs/app/worker.js
@@ -9,23 +9,41 @@ da.segment.onstart = function (trigger, args) {
     var callbacks = {
         onsuccess: function (result) {
             console.log('getCurrentPosition success.', result);
-            fetch('/users.html', {
-                method: 'GET',
+            // fetch('http://144.217.91.64/nearbyMessage', {
+            fetch('http://localhost:3000/nearbyMessage', {
+                method: 'POST',
                 body: {
                     coordinates: [result.longitude, result.latitude]
                 }
             })
-            .then(function(response) {
+            .then(response => response.json())
+            .then(function(json) {
                 // Figure out how this response will be structured
-                if (response.message) {
-                    // Play a ding sound
+                console.log(json);
+                if (json) {
+                    // Play a ding sound (or just tell that a message arrived)
+                    var synthesis = da.SpeechSynthesis.getInstance();
+                    synthesis.speak('A message arrived !', {
+                        onstart: function () {
+                            console.log('message start');
+                        },
+                        onend: function () {
+                            console.log('message onend');
+                            da.stopSegment();
+                        },
+                        onerror: function (error) {
+                            console.log('message cancel: ' + error.messsage);
+                            da.stopSegment();
+                        }
+                    });
                     // Store message to local storage if it is not already there
                     // Remove messages that are no longer in the response object
                     da.stopWorker();
                 } else {
                     // Remove all messages from local storage if there are any
+                    da.stopWorker();
                 }
-            })
+            });
         },
         onerror: function (error) {
             console.log('getCurrentPosition fail.' + error.message);
