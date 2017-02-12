ca2510a20b853271b7f90f7c51a1228062fcd5eb
Louis Bergeron <louispvb@gmail.com>
2017-02-12 14:17:38 -0800

    Add auto location centering to frontend

--- client/client/src/components/BreadcrumbMap.js
+++ client/client/src/components/BreadcrumbMap.js
@@ -2,16 +2,25 @@ import _ from 'lodash';
 import React from 'react';
 import { GoogleMap, Marker, withGoogleMap, InfoWindow } from 'react-google-maps';
 import AddLocationDialog from './AddLocationDialog';
+import canUseDOM from "can-use-dom";
+import raf from "raf";
+
+const geolocation = (
+  canUseDOM && navigator.geolocation ?
+  navigator.geolocation : 
+  ({
+    getCurrentPosition(success, failure) {
+      failure(`Your browser doesn't support geolocation.`);
+    },
+  })
+);
 
 const ShowMap = withGoogleMap(props => (
   <GoogleMap
     ref={props.onMapLoad}
     defaultZoom={17}
     defaultCenter={{ lat: -37.783591, lng: 122.408949 }}
-    center={{
-      lat: Number(props.currentLocation.latitude),
-      lng: Number(props.currentLocation.longitude)
-    }}
+    center={props.center}
     onClick={props.onMapClick}>
     {props.markers.map((marker, index) => (
       <Marker key={index}
@@ -32,6 +41,55 @@ const ShowMap = withGoogleMap(props => (
 export default class BreadcrumbMap extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      center: null,
+      content: null,
+      radius: 6000,
+    };
+  }
+
+  isUnmounted = false;
+
+  componentDidMount() {
+    const tick = () => {
+      if (this.isUnmounted) {
+        return;
+      }
+      this.setState({ radius: Math.max(this.state.radius - 20, 0) });
+
+      if (this.state.radius > 200) {
+        raf(tick);
+      }
+    };
+    geolocation.getCurrentPosition((position) => {
+      if (this.isUnmounted) {
+        return;
+      }
+      this.setState({
+        center: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        },
+        content: `Location found using HTML5.`,
+      });
+
+      raf(tick);
+    }, (reason) => {
+      if (this.isUnmounted) {
+        return;
+      }
+      this.setState({
+        center: {
+          lat: 60,
+          lng: 105,
+        },
+        content: `Error: The Geolocation service failed (${reason}).`,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.isUnmounted = true;
   }
 
   handleMapLoad(map) {
@@ -48,6 +106,7 @@ export default class BreadcrumbMap extends React.Component {
           mapElement={
             <div style={{ height: '100%' }} />
           }
+          center={this.state.center}
           onMapLoad={this.handleMapLoad.bind(this)}
           onMapClick={ this.props.onMapClick }
           markers={ this.props.locations }
